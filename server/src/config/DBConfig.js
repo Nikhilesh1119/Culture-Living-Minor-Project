0237e615e7fd7c4955584132c5f25e1398ec7406
Nikhilesh1119 <nikhileshchouhan20685@acropolis.in>
2023-06-13 21:39:36 +0530

    verify working

--- server/src/config/DBConfig.js
+++ server/src/config/DBConfig.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 // const ATLAS_URL = 'mongodb+srv://kuldeeppanwar460:F4MjNXrHr4AhsHcf@cluster0.zlwvwxo.mongodb.net/?retryWrites=true&w=majority';
+import "dotenv/config.js";
+
 export async function configDB(){
     try {
         mongoose.set('strictQuery',true);
-        await mongoose.connect('mongodb://127.0.0.1:27017/homestay');
+        await mongoose.connect(process.env.DB);
         // await mongoose.connect(ATLAS_URL);
         console.log("Database Connected!"); 
     } catch (error) {
