632843d5f87bd8238335340e5cea9cf33e7d2e2b
Nikhilesh1119 <nikhileshchouhan20685@acropolis.in>
2023-06-20 18:46:17 +0530

    database update

--- server/src/config/DBConfig.js
+++ server/src/config/DBConfig.js
@@ -1,12 +1,9 @@
-import mongoose from "mongoose";
-// const ATLAS_URL = 'mongodb+srv://kuldeeppanwar460:F4MjNXrHr4AhsHcf@cluster0.zlwvwxo.mongodb.net/?retryWrites=true&w=majority';
-import "dotenv/config.js";
+import mongoose from "mongoose"
 
 export async function configDB(){
     try {
         mongoose.set('strictQuery',true);
-        await mongoose.connect(process.env.DB);
-        // await mongoose.connect(ATLAS_URL);
+        await mongoose.connect('mongodb+srv://nikhilesh1119:Nikhilesh%402405@myprojects.ej0eprz.mongodb.net/?retryWrites=true&w=majority');
         console.log("Database Connected!"); 
     } catch (error) {
         console.log(error);
